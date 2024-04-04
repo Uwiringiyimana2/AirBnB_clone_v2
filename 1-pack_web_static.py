cf17322b455bc34c00ecdd6adb09d7879c2d91ae
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-04-04 06:00:06 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,12 +8,12 @@ import os.path
 def do_pack():
     """Fabric script that generates a .tgz archive"""
     dt = datetime.utcnow()
-    filename = "versions/web_static_{}{}{}{}{}{}{}.tgz".format(dt.year,
-                                                               dt.month,
-                                                               dt.day,
-                                                               dt.hour,
-                                                               dt.minute,
-                                                               dt.second)
+    filename = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                             dt.month,
+                                                             dt.day,
+                                                             dt.hour,
+                                                             dt.minute,
+                                                             dt.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
